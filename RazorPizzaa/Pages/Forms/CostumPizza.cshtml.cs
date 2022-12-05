5f260136a5cd3fff4cb260917c8e4883e0bcf6cb
Edanur <edanurrsengur@users.noreply.github.com>
2022-12-05 13:27:09 +0300

    Sayfalar, linkler eklendi

---
+++ RazorPizzaa/Pages/Forms/CostumPizza.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizzaa.Pages.Forms
+{
+    public class CostumPizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
