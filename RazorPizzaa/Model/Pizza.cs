76aa20b01ee46ed72b6c41c3afc8281e8137a5da
Edanur <edanurrsengur@users.noreply.github.com>
2022-12-05 15:02:35 +0300

    Create Model

---
+++ RazorPizzaa/Model/Pizza.cs
@@ -0,0 +1,12 @@
+﻿namespace RazorPizzaa.Model
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string ImageTitle { get; set; }
+        public float BasePrice { get; set; } = 10;
+        public bool Sauce{ get; set; }
+        public bool Cheese { get; set; }
+        public bool Peporoni { get; set; }
+    }
+}
