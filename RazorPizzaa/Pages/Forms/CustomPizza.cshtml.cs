7c644427f5e45d4f723d8ce93e8d785094cf5072
Edanur <edanurrsengur@users.noreply.github.com>
2022-12-06 13:02:11 +0300

    Pizza Sales sayfasında listeleme tamamlandı

--- RazorPizzaa/Pages/Forms/CustomPizza.cshtml.cs
+++ RazorPizzaa/Pages/Forms/CustomPizza.cshtml.cs
@@ -8,22 +8,22 @@ namespace RazorPizzaa.Pages.Forms
     {
         [BindProperty]
         public Pizza Pizza { get; set; }
-        public float FinalPrice { get; set; }
+        public float PizzaPrice { get; set; }
         public void OnGet()
         {
         }
 
         public IActionResult OnPost()
         {
-            FinalPrice = Pizza.BasePrice;
-            FinalPrice += Pizza.Sauce ? 5 : 0;
-            FinalPrice += Pizza.Cheese ? 5 : 0;
-            FinalPrice += Pizza.Peporoni ? 5 : 0;
-            FinalPrice += Pizza.Mushroom ? 5 : 0;
-            FinalPrice += Pizza.Tuna ? 5 : 0;
-            FinalPrice += Pizza.Pineapple? 5 : 0;
-            FinalPrice += Pizza.Beef ? 10 : 0;
-                    return RedirectToPage("/Checkout/Checkout", new { Pizza.Name,FinalPrice=Pizza.FinalPrice, });
+            PizzaPrice = Pizza.BasePrice;
+            PizzaPrice += Pizza.Sauce ? 5 : 0;
+            PizzaPrice += Pizza.Cheese ? 5 : 0;
+            PizzaPrice += Pizza.Peporoni ? 5 : 0;
+            PizzaPrice += Pizza.Mushroom ? 5 : 0;
+            PizzaPrice += Pizza.Tuna ? 5 : 0;
+            PizzaPrice += Pizza.Pineapple? 5 : 0;
+            PizzaPrice += Pizza.Beef ? 10 : 0;
+                    return RedirectToPage("/Checkout/Checkout", new { Pizza.Name,PizzaPrice, });
         }
     }
 }
