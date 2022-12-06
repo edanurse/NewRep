59579fe5bbb48566d40d030aea7899899c704221
Edanur <edanurrsengur@users.noreply.github.com>
2022-12-06 10:15:53 +0300

    Custom pizza page model tamamlandı

---
+++ RazorPizzaa/Pages/Forms/CustomPizza.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzaa.Model;
+
+namespace RazorPizzaa.Pages.Forms
+{
+    public class CustomPizzaModel : PageModel
+    {
+        [BindProperty]
+        public Pizza Pizza { get; set; }
+        public float PizzaPrice { get; set; }
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+         
+            PizzaPrice = Pizza.Sauce ? 5 : 0;
+            PizzaPrice = Pizza.Cheese ? 5 : 0;
+            PizzaPrice = Pizza.Peporoni ? 5 : 0;
+            return RedirectToPage("/Checkout/Checkout", new { Pizza.Name,PizzaPrice });
+        }
+    }
+}
