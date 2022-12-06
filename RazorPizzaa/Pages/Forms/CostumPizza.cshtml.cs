cbcfe6f1a0814ef8854ac3ed5ab5bf9a260eefa4
Edanur <edanurrsengur@users.noreply.github.com>
2022-12-06 09:30:50 +0300

    Custom Pizza page view tamamlandı

--- RazorPizzaa/Pages/Forms/CostumPizza.cshtml.cs
+++ RazorPizzaa/Pages/Forms/CostumPizza.cshtml.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzaa.Model;
 
 namespace RazorPizzaa.Pages.Forms
 {
     public class CostumPizzaModel : PageModel
     {
+        [BindProperty]
+        public Pizza Pizza { get; set; }
         public void OnGet()
         {
         }
